7f704417550c6c54cf9ca4c551f6d947b1164218
Pedro Ferrari Barbosa <pedro.ferraribarbosa2007@gmail.com>
2025-07-06 16:04:13 -0300

    Added movement, stage and acceletations

--- GameRust/src/main.rs
+++ GameRust/src/main.rs
@@ -2,54 +2,113 @@ use macroquad::prelude::*;
 
 struct Player {
     rect: Rect,
+    vel_x: f32,
+    vel_y: f32,
     speed: f32,
+    jump_height: f32,
+    is_falling: bool,
+}
+
+struct World {
+
 }
 
 impl Player {
     fn new() -> Self {
         Self {
             rect: Rect::new(100.0, 100.0, 32.0, 32.0),
-            speed: 5.0,
+            vel_x: 0.0,
+            vel_y: 0.0,
+            speed: 4.0,
+            jump_height: 6.0,
+            is_falling: false,
         }
     }
 
     fn update(&mut self) {
-        let mut move_x = 0.0;
-        let mut move_y = 0.0;
+        let vel_x = &mut self.vel_x;
+        let vel_y = &mut self.vel_y;
 
+        const GRAVITY: f32 = -0.15;
+        const AIRFRICTION: f32 = -0.2;
+
+        // draw line
+        let line_begin: Vec2 =  vec2(self.rect.x + self.rect.w / 2.0, self.rect.y + self.rect.h / 2.0);
+        let line_end: Vec2 = mouse_position().into();
+
+        draw_line(line_begin.x, line_begin.y, line_end.x, line_end.y, 1.0, RED);
+
+        // check if falling
+        if self.rect.y < 550.0 - self.rect.h{
+            self.is_falling = true;
+        } else{
+            self.is_falling = false;
+        }
+
+        // apply accelerations
+        if *vel_x > 0.0 && *vel_x < 1.0 {*vel_x = 0.0}
+        if *vel_x >= 0.0{*vel_x += AIRFRICTION}
+        if *vel_x < 0.0{*vel_x -= AIRFRICTION}
+
+        if self.is_falling{*vel_y += GRAVITY} else{*vel_y = 0.0};
+
+        // movement logic
         let keys_down = get_keys_down();
 
         for key in keys_down {
             match key {
-                KeyCode::D => move_x += self.speed,
-                KeyCode::A => move_x -= self.speed,
-                KeyCode::S => move_y += self.speed,
-                KeyCode::W => move_y -= self.speed,
+                KeyCode::D => *vel_x = self.speed,
+                KeyCode::A => *vel_x = -self.speed,
+                KeyCode::W => if !self.is_falling{*vel_y = self.jump_height},
                 _ => {} 
             }
         }
-
-        self.rect.x += move_x;
-        self.rect.y += move_y;
+        
+        // apply changes to player rect
+        self.rect.x += *vel_x;
+        self.rect.y -= *vel_y;
+        // debug
+        draw_text(&format!("X: {}", vel_x), 20.0, 20.0, 20.0, WHITE);
+        draw_text(&format!("Y: {}", vel_y), 20.0, 40.0, 20.0, WHITE);
+        draw_text(&format!("Is Falling: {}", self.is_falling), 20.0, 60.0, 20.0, WHITE);
     }
 
     fn draw(&self) {
+        // draw player
         draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, BLUE);
+    }
+}
+
+impl World {
+    fn new() -> Self {
+        Self {
+        
+        }
+    }
+
+    fn update(&mut self) {
+       
+    }
+
+    fn draw(&self) {
+        draw_rectangle(-200.0, 550.0, 1000.0, 100.0, GREEN);
     }
 }
 
 #[macroquad::main("Jogo 2D Simples")]
 async fn main() {
     let mut player = Player::new();
+    let world = World::new();
 
     loop {
         clear_background(BLACK);
 
+        // handle world
+        world.draw();
+
+        // handle player
         player.update();
         player.draw();
-
-        // Mostra FPS no canto
-        draw_text(&format!("FPS: {}", get_fps()), 10.0, 20.0, 20.0, WHITE);
 
         next_frame().await;
     }
